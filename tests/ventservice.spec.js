6ab76087afe08d6eb805caeed103dda17aadcd7e
kai <chenkaiser@gmail.com>
2017-05-13 00:23:47 -0400

    more test case,  add async lib

--- tests/ventservice.spec.js
+++ tests/ventservice.spec.js
@@ -4,8 +4,8 @@ const expect = require('chai').expect
 const ventService = require('../services/ventservice')
 
 describe('The vent service module', function () {
-  var insertedId= null;
-  it('saves the vent', function() {
+  var insertedId = null;
+  it('saves the vent', function () {
     var ventObj = {
       Title: "vent 1",
       Content: "This is vent 1",
@@ -13,13 +13,14 @@ describe('The vent service module', function () {
       LikeIt: [{
         Email: 'admin@hupengs.com',
         Nickname: 'admin'
-      },
-      {
-        Email: 'user@hupengs.com',
-        Nickname: 'user'
-      },
+      }
       ],
-      ReportIt: [],
+      ReportIt: [
+        {
+          Email: 'user@hupengs.com',
+          Nickname: 'user'
+        }
+      ],
       Status: 1,
       User: {
         Email: 'admin@hupengs.com',
@@ -38,25 +39,41 @@ describe('The vent service module', function () {
       expect(savedObj).to.not.null;
     });
   });
-  
-  it ('find all vents',function(){
-    return ventService.fetchAllVents('30068','admin',1,10).then
-    (function(vents){
-      expect(vents.length).to.eql(1);
+
+  it('like a vent', function () {
+    return ventService.likeVent(insertedId, { Email: 'test1@hupeng.com', Nickname: 'tester1' }).then(function (obj) {
+      expect(obj.LikeIt[1].Email).to.eql('test1@hupeng.com');
+      expect(obj.LikeIt[1].Nickname).to.eql('tester1');
+    });
+
+  });
+
+  it('report a vent', function () {
+    return ventService.reportVent(insertedId, { Email: 'test2@hupeng.com', Nickname: 'tester2' }).then(function (obj) {
+      expect(obj.ReportIt[1].Email).to.eql('test2@hupeng.com');
+      expect(obj.ReportIt[1].Nickname).to.eql('tester2');
     });
   });
 
 
-  it('delete by id',function(){
+  it('find all vents', function () {
+    return ventService.fetchAllVents('30068', 'admin', 1, 10).then
+      (function (vents) {
+        expect(vents.length).to.eql(1);
+      });
+  });
 
-    return ventService.deleteVent(insertedId).then(function(){
-       ventService.findById(insertedId).then(function(obj){
-          expect(obj).to.be.null;
-       })
+
+  it('delete by id', function () {
+
+    return ventService.deleteVent(insertedId).then(function () {
+      ventService.findById(insertedId).then(function (obj) {
+        expect(obj).to.be.null;
+      })
     });
   });
 
-  it('get the vent', function(){
+  it('get the vent', function () {
 
     var ventObj = {
       Title: "vent 2",
@@ -82,12 +99,11 @@ describe('The vent service module', function () {
         PostalCode: '30068',
       }
     };
-    ventService.saveVent(ventObj).then(function(obj){
-      return ventService.findById(obj._id).then(function(savedObj){
-        expect(savedObj).to.not.null;
-        ventService.deleteVent(obj._id).then(function(){
-          
-        })
+    ventService.saveVent(ventObj).then(function (obj) {
+      ventService.findById(obj._id).then(function (savedObj) {
+        expect(savedObj).to.not.null; 
+        return ventService.deleteVent(savedObj._id).then(function(vent){
+        });
       });
     })
   });
